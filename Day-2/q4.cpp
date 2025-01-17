32d0f92198b92708a98b3cd81a764e6400dd24b9
Shivam <kumarshivam3788@gmail.com>
2025-01-17 18:28:11 +0530

    add: implement area calculation for a circle

---
+++ Day-2/q4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+#define PI 3.14
+
+int main(){
+    float r;
+    cout<<"Enter the radius :";
+    cin>>r;
+    cout<<"Area of Circle :"<<PI*r*r;
+    return 0;
+}
