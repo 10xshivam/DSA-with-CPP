51b2d3dd81bd3de6a66ee679accb09389b9e1798
Shivam <kumarshivam3788@gmail.com>
2025-01-21 01:33:32 +0530

    add: implement star pattern generator in C++

---
+++ Day-6/practice.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // BOX PATTERN
+    // int n = 4;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         cout << i << " ";
+    //     }
+
+    //     cout << endl;
+        
+    // }
+
+    //STAR PATTERN 
+    int n = 6;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
